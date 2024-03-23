83722c6a97cbc81dbe337209e55970241ee91644
Alberto De Pena <albertodepena89@gmail.com>
2024-03-22 21:56:43 -0500

    Using logger category name

--- src/RouteHandlers/IndexHandler.fs
+++ src/RouteHandlers/IndexHandler.fs
@@ -9,14 +9,17 @@ open Microsoft.AspNetCore.Http
 [<RequireQualifiedAccess>]
 module IndexHandler =
 
+    [<Literal>]
+    let LoggerCategoryName = "Index"
+
     let handle: RouteHandler =
         handleRoute (fun httpContext ->
             task {
                 let correlationId = Guid.NewGuid() |> fun guid -> guid.ToString()
 
-                let logger = httpContext.GetLogger "IndexHandler"
+                let logger = httpContext.GetLogger LoggerCategoryName
                 logger.LogInformation("Requesting index view: CorrelationID {CorrelationId}", correlationId)
-                
+
                 let htmlContent = Html.load "Views/Index.html" |> Html.render
                 return Results.Html htmlContent
             })
