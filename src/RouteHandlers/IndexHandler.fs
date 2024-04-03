1ae4f2683143db13e9bf8b976ba255de3b29e4ae
Alberto De Pena <alberto.depena@craneww.com>
2024-04-03 07:57:41 -0500

    Handle htmx request

--- src/RouteHandlers/IndexHandler.fs
+++ src/RouteHandlers/IndexHandler.fs
@@ -37,135 +37,150 @@ module IndexHandler =
     let handleNextTextSample: RouteHandler =
         handleRoute (fun httpContext ->
             task {
-                let filterOption =
-                    httpContext.Request.TryGetQueryStringValue "filter"
-                    |> Option.bind Filter.OfString
+                if httpContext.Request.IsHtmx() then
+                    let filterOption =
+                        httpContext.Request.TryGetQueryStringValue "filter"
+                        |> Option.bind Filter.OfString
 
-                match filterOption with
-                | None -> return failwith "filter is required"
-                | Some filter ->
-                    let textSampleDb = httpContext.GetService<TextSampleDatabase>()
+                    match filterOption with
+                    | None -> return failwith "filter is required"
+                    | Some filter ->
+                        let textSampleDb = httpContext.GetService<TextSampleDatabase>()
 
-                    textSampleDb.SetFilter filter
+                        textSampleDb.SetFilter filter
 
-                    let textSample = textSampleDb.GetNextTextSample()
+                        let textSample = textSampleDb.GetNextTextSample()
 
-                    let htmlContent =
-                        TextSampleComponent.render
-                            { ElementId = PageView.ElementId.TextSample
-                              TextSample = textSample }
+                        let htmlContent =
+                            TextSampleComponent.render
+                                { ElementId = PageView.ElementId.TextSample
+                                  TextSample = textSample }
 
-                    return Results.Html htmlContent
+                        return Results.Html htmlContent
+                else
+                    return Results.Redirect("/")
             })
 
     let handleAddLabel: RouteHandler =
         handleRoute (fun httpContext ->
             task {
-                let labelIdOption =
-                    httpContext.Request.TryGetQueryStringValue "labelId"
-                    |> Option.map Int32.TryParse
-                    |> Option.bind Option.ofPair
+                if httpContext.Request.IsHtmx() then
+                    let labelIdOption =
+                        httpContext.Request.TryGetQueryStringValue "labelId"
+                        |> Option.map Int32.TryParse
+                        |> Option.bind Option.ofPair
 
-                match labelIdOption with
-                | None -> return failwith "labelId is required"
-                | Some labelId ->
-                    let textSampleDb = httpContext.GetService<TextSampleDatabase>()
-                    let labelDb = httpContext.GetService<LabelDatabase>()
-                    let labelOption = labelDb.GetLabelById labelId
+                    match labelIdOption with
+                    | None -> return failwith "labelId is required"
+                    | Some labelId ->
+                        let textSampleDb = httpContext.GetService<TextSampleDatabase>()
+                        let labelDb = httpContext.GetService<LabelDatabase>()
+                        let labelOption = labelDb.GetLabelById labelId
 
-                    match labelOption with
-                    | None -> return failwith "Label not found"
-                    | Some label ->
-                        let textSample = textSampleDb.GetCurrentTextSample()
+                        match labelOption with
+                        | None -> return failwith "Label not found"
+                        | Some label ->
+                            let textSample = textSampleDb.GetCurrentTextSample()
 
-                        let updatedTextSample =
-                            { textSample with
-                                Labels = textSample.Labels @ [ label ] |> List.distinct }
+                            let updatedTextSample =
+                                { textSample with
+                                    Labels = textSample.Labels @ [ label ] |> List.distinct }
 
-                        textSampleDb.UpdateTextSample(updatedTextSample.Id, updatedTextSample)
+                            textSampleDb.UpdateTextSample(updatedTextSample.Id, updatedTextSample)
 
-                        let htmlContent =
-                            TextSampleComponent.render
-                                { ElementId = PageView.ElementId.TextSample
-                                  TextSample = updatedTextSample }
+                            let htmlContent =
+                                TextSampleComponent.render
+                                    { ElementId = PageView.ElementId.TextSample
+                                      TextSample = updatedTextSample }
 
-                        return Results.Html htmlContent
+                            return Results.Html htmlContent
+                else
+                    return Results.Redirect("/")
             })
 
     let handleRemoveLabel: RouteHandler =
         handleRoute (fun httpContext ->
             task {
-                let labelIdOption =
-                    httpContext.Request.TryGetQueryStringValue "labelId"
-                    |> Option.map Int32.TryParse
-                    |> Option.bind Option.ofPair
+                if httpContext.Request.IsHtmx() then
+                    let labelIdOption =
+                        httpContext.Request.TryGetQueryStringValue "labelId"
+                        |> Option.map Int32.TryParse
+                        |> Option.bind Option.ofPair
 
-                match labelIdOption with
-                | None -> return failwith "labelId is required"
-                | Some labelId ->
-                    let textSampleDb = httpContext.GetService<TextSampleDatabase>()
-                    let labelDb = httpContext.GetService<LabelDatabase>()
-                    let labelOption = labelDb.GetLabelById labelId
+                    match labelIdOption with
+                    | None -> return failwith "labelId is required"
+                    | Some labelId ->
+                        let textSampleDb = httpContext.GetService<TextSampleDatabase>()
+                        let labelDb = httpContext.GetService<LabelDatabase>()
+                        let labelOption = labelDb.GetLabelById labelId
 
-                    match labelOption with
-                    | None -> return failwith "Label not found"
-                    | Some label ->
-                        let textSample = textSampleDb.GetCurrentTextSample()
+                        match labelOption with
+                        | None -> return failwith "Label not found"
+                        | Some label ->
+                            let textSample = textSampleDb.GetCurrentTextSample()
 
-                        let updatedTextSample =
-                            { textSample with
-                                Labels = textSample.Labels |> List.filter (fun x -> x.Id <> label.Id) }
+                            let updatedTextSample =
+                                { textSample with
+                                    Labels = textSample.Labels |> List.filter (fun x -> x.Id <> label.Id) }
 
-                        textSampleDb.UpdateTextSample(updatedTextSample.Id, updatedTextSample)
+                            textSampleDb.UpdateTextSample(updatedTextSample.Id, updatedTextSample)
 
-                        let htmlContent =
-                            TextSampleComponent.render
-                                { ElementId = PageView.ElementId.TextSample
-                                  TextSample = updatedTextSample }
+                            let htmlContent =
+                                TextSampleComponent.render
+                                    { ElementId = PageView.ElementId.TextSample
+                                      TextSample = updatedTextSample }
 
-                        return Results.Html htmlContent
+                            return Results.Html htmlContent
+                else
+                    return Results.Redirect("/")
             })
 
     let handleFilter: RouteHandler =
         handleRoute (fun httpContext ->
             task {
-                let filterOption =
-                    httpContext.Request.TryGetQueryStringValue "filter"
-                    |> Option.bind Filter.OfString
+                if httpContext.Request.IsHtmx() then
+                    let filterOption =
+                        httpContext.Request.TryGetQueryStringValue "filter"
+                        |> Option.bind Filter.OfString
 
-                match filterOption with
-                | None -> return failwith "filter is required"
-                | Some filter ->
-                    let textSampleDb = httpContext.GetService<TextSampleDatabase>()
+                    match filterOption with
+                    | None -> return failwith "filter is required"
+                    | Some filter ->
+                        let textSampleDb = httpContext.GetService<TextSampleDatabase>()
 
-                    textSampleDb.SetFilter filter
+                        textSampleDb.SetFilter filter
 
-                    let textSample = textSampleDb.GetCurrentTextSample()
+                        let textSample = textSampleDb.GetCurrentTextSample()
 
-                    let htmlContent =
-                        TextSampleComponent.render
-                            { ElementId = PageView.ElementId.TextSample
-                              TextSample = textSample }
+                        let htmlContent =
+                            TextSampleComponent.render
+                                { ElementId = PageView.ElementId.TextSample
+                                  TextSample = textSample }
 
-                    return Results.Html htmlContent
+                        return Results.Html htmlContent
+                else
+                    return Results.Redirect("/")
             })
 
     let handleSearchLabels: RouteHandler =
         handleRoute (fun httpContext ->
             task {
-                let filterOption = httpContext.Request.TryGetQueryStringValue "label-filter"
+                if httpContext.Request.IsHtmx() then
+                    let filterOption = httpContext.Request.TryGetQueryStringValue "label-filter"
 
-                match filterOption with
-                | None -> return failwith "filter is required"
-                | Some filter ->
-                    let labelDb = httpContext.GetService<LabelDatabase>()
+                    match filterOption with
+                    | None -> return failwith "filter is required"
+                    | Some filter ->
+                        let labelDb = httpContext.GetService<LabelDatabase>()
 
-                    let labels = labelDb.SearchLabels(filter)
+                        let labels = labelDb.SearchLabels(filter)
 
-                    let htmlContent =
-                        SelectLabelComponent.render
-                            { ElementId = PageView.ElementId.LabelDataSource
-                              Labels = labels }
+                        let htmlContent =
+                            SelectLabelComponent.render
+                                { ElementId = PageView.ElementId.LabelDataSource
+                                  Labels = labels }
 
-                    return Results.Html htmlContent
+                        return Results.Html htmlContent
+                else
+                    return Results.Redirect("/")
             })
